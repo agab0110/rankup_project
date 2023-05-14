dbb21964699e55f0b1a9e16fe1ab250265bba8b4
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-14 12:31:57 +0200

    added function list rule

--- src/main/java/com/ing/rankup_b/rule/RuleController.java
+++ src/main/java/com/ing/rankup_b/rule/RuleController.java
@@ -1,7 +1,10 @@
 package com.ing.rankup_b.rule;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,11 @@ public class RuleController {
 
     public RuleController(RuleService service) {
         this.service = service;
+    }
+
+    @GetMapping(path = "/rules/{id_team}")
+    public ResponseEntity ListRule(@PathVariable int id_team) {
+        return this.service.ListRule(id_team);
+        
     }
 }
--- src/main/java/com/ing/rankup_b/rule/RuleService.java
+++ src/main/java/com/ing/rankup_b/rule/RuleService.java
@@ -1,6 +1,11 @@
 package com.ing.rankup_b.rule;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +16,19 @@ public class RuleService {
 
     public RuleService(RuleRepository repository) {
         this.repository = repository;
+    }
+    public ResponseEntity ListRule(int codice){
+        List<Rule> rules = new ArrayList<>();
+        
+        for (Rule rule : (List<Rule>)this.repository.findAll()) {
+            if (rule.getTeam().getCodice() == codice) {
+                rules.add(rule);
+            }
+        }
+        if (rules.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Non ci sono regole per questo team");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(rules);
+
     }
 }
