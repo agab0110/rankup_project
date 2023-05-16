019eeaadcf2c367b27c4c4e4a9f09768a939b3b5
Paolo <paolodibiase1@gmail.com>
2023-05-16 11:11:10 +0200

    deleted all * import

--- src/main/java/com/ing/rankup_b/ruleCompleted/RuleCompletedController.java
+++ src/main/java/com/ing/rankup_b/ruleCompleted/RuleCompletedController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("ruleCompletedApi")
