bab750b8f0cc78030c356405a06fe09be882ff43
agab0110 <agabrielex01@gmail.com>
2023-05-17 17:01:39 +0200

    added path variables

--- src/main/java/com/ing/rankup_b/taskCompleted/TaskCompletedController.java
+++ src/main/java/com/ing/rankup_b/taskCompleted/TaskCompletedController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("taskCompletedApi")
+@RequestMapping("/taskCompletedApi")
 @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8200", "http://localhost:4200"})
 
 public class TaskCompletedController {
@@ -44,7 +44,7 @@ public class TaskCompletedController {
     }
 
     @GetMapping(path = "/getTaskForSpecificUser/{idTeam}/{idUser}")
-    public ResponseEntity getTaskForSpecificUser(long idTeam, int idUser) {
+    public ResponseEntity getTaskForSpecificUser(@PathVariable long idTeam, @PathVariable int idUser) {
         return this.service.getTaskForASpecificUser(idTeam, idUser);
     }
 }
