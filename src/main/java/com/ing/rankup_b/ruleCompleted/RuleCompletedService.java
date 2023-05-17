3e2ff23662947116b2ffae8469267e0ac4ccbd23
agab0110 <agabrielex01@gmail.com>
2023-05-17 16:02:53 +0200

    created get rule completed details function

--- src/main/java/com/ing/rankup_b/ruleCompleted/RuleCompletedService.java
+++ src/main/java/com/ing/rankup_b/ruleCompleted/RuleCompletedService.java
@@ -65,21 +65,12 @@ public class RuleCompletedService {
         return this.repository.findUserHistoryT(idTeam, nomeTask);
     }
 
-    public ArrayList<Object> getRuleCompleted(int id_team, int id_user, int id_rule_completed) {
-        ArrayList<String> result = this.repository.ruleCompletedQuery(id_team, id_user, id_rule_completed);
-
-        ArrayList<Object> rulesCompleted = new ArrayList<Object>();
-
-        for (String r: result) {
-            rulesCompleted.add(new Object() {
-                public String nome = r.split(",")[0];
-                public int points = Integer.parseInt(r.split(",")[1]) + (r.split(",")[2].equals("null") ? 0 : Integer.parseInt(r.split(",")[2]));
-                public String description = r.split(",")[3];
-                public String comment = r.split(",")[4];
-                public String username = r.split(",")[5];
-
-            });
-        }
-        return rulesCompleted;
+    /**
+     * Funzione per prendere i dettagli di una regola completata
+     * @param idRuleCompleted l'id della regola completata
+     * @return (200 OK) con la regola completata nel body
+     */
+    public ResponseEntity getRuleCompletedDetails(int idRuleCompleted) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.repository.findById(idRuleCompleted));
     }
 }
