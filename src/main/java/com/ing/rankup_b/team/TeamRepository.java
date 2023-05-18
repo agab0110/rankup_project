a001328f2f5f91f1cadae3411d431b3e993b89d5
agab0110 <agabrielex01@gmail.com>
2023-05-18 17:32:55 +0200

    deleted useless query

--- src/main/java/com/ing/rankup_b/team/TeamRepository.java
+++ src/main/java/com/ing/rankup_b/team/TeamRepository.java
@@ -12,19 +12,6 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @Query(value = "SELECT JSON_ARRAYAGG(JSON_OBJECT('idTeam', team.id_team, 'name:', team.name, 'photo', team.photo)) FROM team WHERE team.privacy = 1 ORDER BY RAND() LIMIT 20;", nativeQuery = true)
     String rand();
 
-    @Query(value = "SELECT user.username, rule.name, rule_completed.timestamp, rule_completed.status FROM rule_completed JOIN user ON rule_completed.id_user = user.id_user JOIN rule ON rule.id_rule = rule_completed.id_rule WHERE rule.team = ?1 AND rule.name LIKE ?2", nativeQuery = true)
-    public ArrayList<String> requestHistoryActivityRuleQuery(int id_team, String activity);
-
-    @Query(value = "SELECT user.username, task.name, task_completed.timestamp, task_completed.status FROM task_completed JOIN user ON task_completed.id_user = user.id_user JOIN task ON task.id_task = task_completed.task_id_task WHERE task.team = ?1 AND task.name LIKE ?2", nativeQuery = true)
-    public ArrayList<String> requestHistoryActivityTaskQuery(int id_team, String activity);
-
-    @Query(value = "SELECT user.username, rule.name, rule_completed.timestamp, rule_completed.status FROM rule_completed JOIN user ON rule_completed.id_user = user.id_user JOIN rule ON rule.id_rule = rule_completed.id_rule WHERE rule.team = ?1 AND rule_completed.revision_date LIKE ?2", nativeQuery = true)
-    public ArrayList<String> requestHistoryDateRuleQuery(int id_team, String date);
-
-    @Query(value = "SELECT user.username, task.name, task_completed.timestamp, task_completed.status FROM task_completed JOIN user ON task_completed.id_user = user.id_user JOIN task ON task.id_task = task_completed.task_id_task WHERE task.team = ?1 AND task_completed.revision_date LIKE ?2", nativeQuery = true)
-    public ArrayList<String> requestHistoryDateTaskQuery(int id_team, String date);
-
-
     @Query(value = "SELECT rule.name, rule.points, rule_completed.bonus_points FROM rule_completed JOIN rule ON rule_completed.id_rule = rule.id_rule JOIN user ON user.id_user = rule_completed.id_user JOIN team ON team.id_team = rule.team WHERE rule_completed.status = 1 AND rule.team = ?1 AND rule_completed.id_user = ?2", nativeQuery = true)
     public ArrayList<String> userCompletedActivitiesRuleQuery(int id_team, int id_user);
 
