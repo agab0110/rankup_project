8b7c9c0a2178b439a9c44cd9d18017d7369fda20
agab0110 <agabrielex01@gmail.com>
2023-05-19 16:12:36 +0200

    fixed end point

--- src/main/java/com/ing/rankup_b/team/TeamController.java
+++ src/main/java/com/ing/rankup_b/team/TeamController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("teamApi")
+@RequestMapping("/teamApi")
 @CrossOrigin(origins = { "http://localhost:8100", "http://localhost:8200", "http://localhost:4200" })
 
 public class TeamController {
@@ -54,21 +54,21 @@ public class TeamController {
         return ResponseEntity.status(HttpStatus.OK).body(teams);
     }
 
-    @GetMapping(path = "user/completedActivities")
+    @GetMapping(path = "/user/completedActivities")
     public ResponseEntity userCompletedActivities(@RequestParam("id_team") int id_team, @RequestParam("id_user") int id_user) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 this.service.getUserCompletedActivities(id_team, id_user)
         );
     }
 
-    @GetMapping(path = "admin/completedActivities")
+    @GetMapping(path = "/admin/completedActivities")
     public ResponseEntity adminCompletedActivities(@RequestParam("id_team") int id_team, @RequestParam("id_user") int id_user) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 this.service.getUserCompletedActivities(id_team, id_user)
         );
     }
 
-    @GetMapping(path = "pendingActivities")
+    @GetMapping(path = "/pendingActivities")
     public ResponseEntity pendingActivities(@RequestParam("id_team") int id_team) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 this.service.getPendingActivities(id_team)
