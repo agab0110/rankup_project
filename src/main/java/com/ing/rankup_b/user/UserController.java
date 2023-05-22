d3467bb41034f124536857002f6a018d1726715d
agab0110 <agabrielex01@gmail.com>
2023-05-22 16:33:38 +0200

    changed url for port 4200

--- src/main/java/com/ing/rankup_b/user/UserController.java
+++ src/main/java/com/ing/rankup_b/user/UserController.java
@@ -15,11 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/userApi")
-@CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8200", "http://localhost/4200"})
+@CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8200", "http://localhost:4200"})
 public class UserController {
 
     @Autowired
