4e2d35077b3f48010161796f235392e3c6634fda
agab0110 <agabrielex01@gmail.com>
2023-05-24 09:12:06 +0200

    added api n 63

--- src/main/java/com/ing/rankup_b/userJoinsTeam/UserJoinsTeamController.java
+++ src/main/java/com/ing/rankup_b/userJoinsTeam/UserJoinsTeamController.java
@@ -2,11 +2,14 @@ package com.ing.rankup_b.userJoinsTeam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +63,14 @@ public class UserJoinsTeamController {
     @GetMapping(path = "/partecipants/{idTeam}")
     public ResponseEntity<?> getPartecipants(@PathVariable long idTeam) {
         return this.service.findPartecipants(idTeam);
+    }
+
+    /**
+     * N.63
+     */
+    @PostMapping(path = "/addUser", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addUser(@RequestBody UserJoinsTeam userJoinsTeam) {
+        return this.service.addUser(userJoinsTeam);
     }
 
 }
