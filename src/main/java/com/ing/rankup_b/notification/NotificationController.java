78310541b36bc9674233011df556e08c286d2c6a
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-24 22:08:30 +0200

    added api n 64

--- src/main/java/com/ing/rankup_b/notification/NotificationController.java
+++ src/main/java/com/ing/rankup_b/notification/NotificationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("notificationApi")
+@RequestMapping("title")
 @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8200", "http://localhost:4200"})
 
 public class NotificationController {
