d08b1315ffb2df4431e4f44188107a07cde65041
agab0110 <agabrielex01@gmail.com>
2023-05-25 11:03:11 +0200

    changed primary key

--- src/main/java/com/ing/rankup_b/userJoinsTeam/UserJoinsTeam.java
+++ src/main/java/com/ing/rankup_b/userJoinsTeam/UserJoinsTeam.java
@@ -3,16 +3,16 @@ package com.ing.rankup_b.userJoinsTeam;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ing.rankup_b.team.Team;
 import com.ing.rankup_b.user.User;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,19 +25,18 @@ public class UserJoinsTeam {
         Sospeso, Accettato, Rifiutato
     }
     
-    @EmbeddedId
-    @Column(name = "key")
-    UserJoinsTeamKey key;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @ManyToOne
     @JoinColumn(name = "id_user")
-    @MapsId("idUser")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "id_team")
-    @MapsId("idTeam")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Team team;
 
