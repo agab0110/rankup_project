7b64334fdb0e0211478daa1a1056ebbecb39b119
agab0110 <agabrielex01@gmail.com>
2023-05-25 12:19:12 +0200

    added new notification end point

--- src/main/java/com/ing/rankup_b/adminReciveNotification/AdminReciveNotificationController.java
+++ src/main/java/com/ing/rankup_b/adminReciveNotification/AdminReciveNotificationController.java
@@ -1,7 +1,10 @@
 package com.ing.rankup_b.adminReciveNotification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,10 @@ public class AdminReciveNotificationController {
 
     public AdminReciveNotificationController(AdminReciveNotificationService service) {
         this.service = service;
+    }
+
+    @PostMapping(path = "newNotification/{idAdmin}/{idNotification}")
+    public ResponseEntity<?> newNotification(@PathVariable int idAdmin, @PathVariable int idNotification) {
+        return this.service.newNotification(idAdmin, idNotification);
     }
 }
