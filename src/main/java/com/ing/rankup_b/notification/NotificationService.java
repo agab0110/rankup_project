c42984f1b59bc8c9953b5eae167c71df91471e1d
agab0110 <agabrielex01@gmail.com>
2023-05-25 10:00:58 +0200

    removed user and team repository

--- src/main/java/com/ing/rankup_b/notification/NotificationService.java
+++ src/main/java/com/ing/rankup_b/notification/NotificationService.java
@@ -7,37 +7,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.ing.rankup_b.team.Team;
-import com.ing.rankup_b.team.TeamRepository;
-import com.ing.rankup_b.user.User;
-import com.ing.rankup_b.user.UserRepository;
-
 @Service
 public class NotificationService {
 
     @Autowired
     private NotificationRepository notificationRepository;
-    private UserRepository userRepository;
-    private TeamRepository teamRepository;
 
-    public NotificationService(NotificationRepository notificationRepository, UserRepository userRepository, TeamRepository teamRepository) {
+    public NotificationService(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
-        this.userRepository = userRepository;
-        this.teamRepository = teamRepository;
+
     }
 
-    public ResponseEntity<?> newNotification(long idTeam, int idUser, Notification notification) {
-        User user = new User();
-        user = this.userRepository.findById(idUser).get();
+    public ResponseEntity<?> newAdminNotification(long idTeam, int idAdmin, Notification notification) {
         
-        Team team = new Team();
-        team = this.teamRepository.findById(idTeam).get();
-
         Date date = new Date();
 
         notification.setDate(date);
-        notification.setUser(user);
-        notification.setTeam(team);
 
         return ResponseEntity.status(HttpStatus.OK).body(this.notificationRepository.save(notification));
     }
