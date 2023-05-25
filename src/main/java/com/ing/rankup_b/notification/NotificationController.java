80fc7951c6b2446307557e54f28a77b4ec8ba5d2
agab0110 <agabrielex01@gmail.com>
2023-05-25 09:35:59 +0200

    added newNotification function

--- src/main/java/com/ing/rankup_b/notification/NotificationController.java
+++ src/main/java/com/ing/rankup_b/notification/NotificationController.java
@@ -1,7 +1,11 @@
 package com.ing.rankup_b.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,9 @@ public class NotificationController {
     public NotificationController(NotificationService service) {
         this.service = service;
     }    
+
+    @PostMapping(path = "/newNotification", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> newNotification(@RequestBody Notification notification) {
+        return this.service.newNotification(notification);
+    }
 }
--- src/main/java/com/ing/rankup_b/notification/NotificationService.java
+++ src/main/java/com/ing/rankup_b/notification/NotificationService.java
@@ -1,6 +1,10 @@
 package com.ing.rankup_b.notification;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +17,9 @@ public class NotificationService {
         this.repository = repository;
     }
 
-    
+    public ResponseEntity<?> newNotification(Notification notification) {
+        Date date = new Date();
+        notification.setDate(date);
+        return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(notification));
+    }
 }
