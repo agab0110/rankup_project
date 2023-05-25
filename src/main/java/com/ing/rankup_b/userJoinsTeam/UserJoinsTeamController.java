3c3d719c10c325072b76888c6051fb50bca2bede
MiniGianluca <gianluca.minichetti@yahoo.com>
2023-05-25 11:24:00 +0200

    mege

--- src/main/java/com/ing/rankup_b/userJoinsTeam/UserJoinsTeamController.java
+++ src/main/java/com/ing/rankup_b/userJoinsTeam/UserJoinsTeamController.java
@@ -2,19 +2,15 @@ package com.ing.rankup_b.userJoinsTeam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/userJoinsTeamApi")
@@ -74,6 +70,9 @@ public class UserJoinsTeamController {
     public ResponseEntity getRequests(@PathVariable long idTeam) {
         return this.service.getrequests(idTeam);
     }
+    /*
+     * N.12
+     */
     @PostMapping(path = "/addUser")
     public ResponseEntity addUser(@RequestParam(value = "idTeam", required = true) long idTeam, @RequestParam(value = "idUser", required = true) int idUser) {
         return service.addUser(idTeam,idUser);
