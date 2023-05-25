f1c431ff4895457ae67c5fe67c5ab61a0b668dbb
agab0110 <agabrielex01@gmail.com>
2023-05-25 12:19:22 +0200

    created new notification function

--- src/main/java/com/ing/rankup_b/adminReciveNotification/AdminReciveNotificationService.java
+++ src/main/java/com/ing/rankup_b/adminReciveNotification/AdminReciveNotificationService.java
@@ -1,15 +1,41 @@
 package com.ing.rankup_b.adminReciveNotification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.ing.rankup_b.adminManageTeam.AdminManageTeam;
+import com.ing.rankup_b.adminManageTeam.AdminManageTeamRepository;
+import com.ing.rankup_b.notification.Notification;
+import com.ing.rankup_b.notification.NotificationRepository;
 
 @Service
 public class AdminReciveNotificationService {
 
     @Autowired
-    private AdminReciveNotificationRepository repository;
+    private AdminReciveNotificationRepository adminReciveNotificationRepository;
 
-    public AdminReciveNotificationService(AdminReciveNotificationRepository repository) {
-        this.repository = repository;
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private AdminManageTeamRepository adminManageTeamRepository;
+
+    public AdminReciveNotificationService(AdminReciveNotificationRepository adminReciveNotificationRepository, NotificationRepository notificationRepository, AdminManageTeamRepository adminManageTeamRepository) {
+        this.adminReciveNotificationRepository = adminReciveNotificationRepository;
+        this.notificationRepository = notificationRepository;
+        this.adminManageTeamRepository = adminManageTeamRepository;
+    }
+
+    public ResponseEntity<?> newNotification(int idAdmin, int idNotification) {
+        AdminManageTeam admin = this.adminManageTeamRepository.findById(idAdmin).get();
+        Notification notification = this.notificationRepository.findById(idNotification).get();
+
+        AdminReciveNotification adminReciveNotification = new AdminReciveNotification();
+        adminReciveNotification.setAdmin(admin);
+        adminReciveNotification.setNotification(notification);
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.adminReciveNotificationRepository.save(adminReciveNotification));
     }
 }
