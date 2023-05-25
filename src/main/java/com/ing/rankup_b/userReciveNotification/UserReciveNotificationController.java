fcc3e62f5cc7b77801f9021dfeac81da56c41cb4
agab0110 <agabrielex01@gmail.com>
2023-05-25 11:10:02 +0200

    changed end point name

--- src/main/java/com/ing/rankup_b/userReciveNotification/UserReciveNotificationController.java
+++ src/main/java/com/ing/rankup_b/userReciveNotification/UserReciveNotificationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("notificationApi")
+@RequestMapping("userReciveNotificationApi")
 @CrossOrigin(origins = {"http://localhost:8100", "http://localhost:8200", "http://localhost:4200"})
 public class UserReciveNotificationController {
     
