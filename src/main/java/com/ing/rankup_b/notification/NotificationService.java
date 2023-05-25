608e0251e0de5dd0523a689c8843cf8d6c8e65bf
agab0110 <agabrielex01@gmail.com>
2023-05-25 09:51:38 +0200

    added newNotification function

--- src/main/java/com/ing/rankup_b/notification/NotificationService.java
+++ src/main/java/com/ing/rankup_b/notification/NotificationService.java
@@ -7,19 +7,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.ing.rankup_b.team.Team;
+import com.ing.rankup_b.team.TeamRepository;
+import com.ing.rankup_b.user.User;
+import com.ing.rankup_b.user.UserRepository;
+
 @Service
 public class NotificationService {
 
     @Autowired
-    private NotificationRepository repository;
+    private NotificationRepository notificationRepository;
+    private UserRepository userRepository;
+    private TeamRepository teamRepository;
 
-    public NotificationService(NotificationRepository repository) {
-        this.repository = repository;
+    public NotificationService(NotificationRepository notificationRepository, UserRepository userRepository, TeamRepository teamRepository) {
+        this.notificationRepository = notificationRepository;
+        this.userRepository = userRepository;
+        this.teamRepository = teamRepository;
     }
 
-    public ResponseEntity<?> newNotification(Notification notification) {
+    public ResponseEntity<?> newNotification(long idTeam, int idUser, Notification notification) {
+        User user = new User();
+        user = this.userRepository.findById(idUser).get();
+        
+        Team team = new Team();
+        team = this.teamRepository.findById(idTeam).get();
+
         Date date = new Date();
+
         notification.setDate(date);
-        return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(notification));
+        notification.setUser(user);
+        notification.setTeam(team);
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.notificationRepository.save(notification));
     }
 }
