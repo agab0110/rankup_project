b77de98bcc6f39514330a4867eaea3831a3f71d2
agab0110 <agabrielex01@gmail.com>
2023-05-25 12:02:50 +0200

    created new notification function

--- src/main/java/com/ing/rankup_b/notification/NotificationService.java
+++ src/main/java/com/ing/rankup_b/notification/NotificationService.java
@@ -7,22 +7,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.ing.rankup_b.team.Team;
+import com.ing.rankup_b.team.TeamRepository;
+
 @Service
 public class NotificationService {
 
     @Autowired
     private NotificationRepository notificationRepository;
 
-    public NotificationService(NotificationRepository notificationRepository) {
-        this.notificationRepository = notificationRepository;
+    @Autowired
+    private TeamRepository teamRepository;
 
+    public NotificationService(NotificationRepository notificationRepository, TeamRepository teamRepository) {
+        this.notificationRepository = notificationRepository;
+        this.teamRepository = teamRepository;
     }
 
-    public ResponseEntity<?> newAdminNotification(long idTeam, int idAdmin, Notification notification) {
-        
+    /**
+     * Funzione per creare una nuova notifica per un determinato team
+     * @param idTeam il team per cui viene creata la notifica
+     * @param notification il body della notifica
+     * @return (200 OK) con la notifica salvata nel body 
+     */
+    public ResponseEntity<?> newNotification(long idTeam, Notification notification) {
+        Team team = this.teamRepository.findById(idTeam).get();
         Date date = new Date();
 
         notification.setDate(date);
+        notification.setTeam(team);
 
         return ResponseEntity.status(HttpStatus.OK).body(this.notificationRepository.save(notification));
     }
