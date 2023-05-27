38b66bd9aa23d137f55fb129a68aafab78abc67a
PSogeki <paolodibiase1@gmail.com>
2023-05-27 09:07:17 +0200

    api

--- src/main/java/com/ing/rankup_b/adminManageTeam/AdminManageTeamController.java
+++ src/main/java/com/ing/rankup_b/adminManageTeam/AdminManageTeamController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,4 +31,11 @@ public class AdminManageTeamController {
         return service.addAdmin(idTeam,idUser);
     }
 
+    /**
+     * N.41
+     */
+    @GetMapping(path = "/teams/{idUser}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getTeams(@PathVariable int idUser) {
+        return this.service.findTeams(idUser);
+    }
 }
--- src/main/java/com/ing/rankup_b/adminManageTeam/AdminManageTeamService.java
+++ src/main/java/com/ing/rankup_b/adminManageTeam/AdminManageTeamService.java
@@ -28,4 +28,25 @@ public class AdminManageTeamService {
         
     }
 
+    /**
+     * Funzione per prendere la lista di team in cui è un utente
+     * 
+     * @param idUser l'utente per cui si vuole la lista dei team
+     * @return (200 OK) con la lista dei team se c'è almeno un elemento nella lista,<br>(400 BAD_REQUEST) altrimenti
+     */
+    public ResponseEntity findTeams(int idUser) {
+        List<Team> teams = new ArrayList<>();
+        for (AdminManageTeam u : this.repository.findAll()) {
+            if(u.getUser().getId() == idUser) {
+                teams.add(u.getTeam());
+            }
+        }
+
+        if(teams.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nessun team trovato");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(teams);
+        }
+    }
+
 }
