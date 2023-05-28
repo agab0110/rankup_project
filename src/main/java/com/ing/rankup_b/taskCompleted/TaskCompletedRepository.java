6805a0cb1c010eb1ad096fea83f83f73dc89fc3a
Alessandro <alessandron925@gmail.com>
2023-05-28 12:00:18 +0200

    bug fix

--- src/main/java/com/ing/rankup_b/taskCompleted/TaskCompletedRepository.java
+++ src/main/java/com/ing/rankup_b/taskCompleted/TaskCompletedRepository.java
@@ -13,6 +13,6 @@ public interface TaskCompletedRepository extends JpaRepository<TaskCompleted, In
     @Query(value = "UPDATE task_completed SET task_completed.revision_date= ?1, task_completed.status= ?2, task_completed.comment= ?3 WHERE task_completed.id_task_completed = ?4 ", nativeQuery = true)
     String update(Timestamp revision_date, int status, String comment, int idTaskCompleted);
 
-    @Query(value = "SELECT JSON_ARRAYAGG(JSON_OBJECT('name', task.name, 'username', user.name, 'photo', user.photo)) FROM task_completed JOIN task ON task.id_task = task_completed.id_task join user on user.id_user = task_completed.id_user WHERE task_completed.status = 0 and task.id_team = ?1", nativeQuery = true)
+    @Query(value = "SELECT JSON_ARRAYAGG(JSON_OBJECT('id_task', task_completed.id_task_completed, 'name', task.name, 'username', user.name, 'photo', user.photo)) FROM task_completed JOIN task ON task.id_task = task_completed.id_task join user on user.id_user = task_completed.id_user WHERE task_completed.status = 0 and task.id_team = ?1", nativeQuery = true)
     String pending(int id_team);
 }
