489143bebd050c0462b41409c414258667ac5aa9
PSogeki <paolodibiase1@gmail.com>
2023-06-02 17:49:24 +0200

    created file table

--- src/main/java/com/ing/rankup_b/file/File.java
+++ src/main/java/com/ing/rankup_b/file/File.java
@@ -1,5 +1,30 @@
 package com.ing.rankup_b.file;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "file")
 public class File {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Lob
+    @Column(name = "picByte")
+    private byte[] picByte;
 }
