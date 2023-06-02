cd91d164b21722289238bcffae9aba4acd32e502
PSogeki <paolodibiase1@gmail.com>
2023-06-02 17:50:45 +0200

    created file table

--- src/main/java/com/ing/rankup_b/file/File.java
+++ src/main/java/com/ing/rankup_b/file/File.java
@@ -17,7 +17,7 @@ public class File {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     
     @Column(name = "name")
     private String name;
